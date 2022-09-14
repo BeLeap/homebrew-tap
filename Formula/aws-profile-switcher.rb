afb4123379ed5fdbe5fd4f9362efb155d9ee0eb9
Changseo <changseo.jang@riiid.co>
2022-09-14 19:45:41 +0900

    aws-profile-switcher 0.1.1 (new formula)
    * apes v0.1.1
    * remove template comments
    * fix test
    
    Closes #1.
    
    Signed-off-by: Changseo Jang <beleap@beleap.dev>

---
+++ Formula/aws-profile-switcher.rb
@@ -0,0 +1,17 @@
+class AwsProfileSwitcher < Formula
+  desc "AWS_PROFILE switcher"
+  homepage "https://github.com/BeLeap/aws-profile-switcher"
+  url "https://github.com/BeLeap/aws-profile-switcher/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "ee207e5cd425220e746b94fbceec63fbd76326a9c0d18834011ae7630664f2c9"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "apes", "-h"
+  end
+end
