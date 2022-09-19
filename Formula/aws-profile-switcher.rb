961d47dc6936be88e500e250c2293843ef5ea96b
Changseo Jang <beleap@beleap.dev>
2022-09-19 04:17:39 +0000

    aws-profile-switcher: update 0.1.6 bottle.

--- Formula/aws-profile-switcher.rb
+++ Formula/aws-profile-switcher.rb
@@ -7,9 +7,9 @@ class AwsProfileSwitcher < Formula
   head "https://github.com/BeLeap/aws-profile-switcher.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/BeLeap/homebrew-tap/releases/download/aws-profile-switcher-0.1.5"
-    sha256 cellar: :any_skip_relocation, big_sur:      "09a77b234f235e8c8fb18219a73f636f01f76bf1cd2d588e394a010a55ab30c5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3d8511b70ba475842759e17e145216b3b4cc82ea67cf90f023daed8707ecde70"
+    root_url "https://github.com/BeLeap/homebrew-tap/releases/download/aws-profile-switcher-0.1.6"
+    sha256 cellar: :any_skip_relocation, big_sur:      "8dbecd7194481d47a6c6f078ca0b6b92fa77cdb3ca4b625e18b8088524efd6ae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4d7d20935f8abda11b43935049ae5ea2e5b6d3018fa4e334448a412b9ea489fa"
   end
 
   depends_on "rust" => :build
