8989b2b69f33a5cfebdf78dd317efc9883ee1fca
BeLeap <beleap@beleap.dev>
2024-04-28 15:35:14 +0900

    fix: description

--- Formula/teleproxy.rb
+++ Formula/teleproxy.rb
@@ -1,5 +1,5 @@
 class Teleproxy < Formula
-  desc "spy and proxy tele-location"
+  desc "Spy and proxy http request on tele-location"
   homepage "https://github.com/BeLeap/teleproxy"
   url "https://github.com/BeLeap/teleproxy/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "9b4c86ef8595c3d6aa53ed31f397ea7cbabd72022d9000a1049673983c85e7c4"
