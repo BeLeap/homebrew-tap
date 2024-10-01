e4ed4294c3b85fe15f70822271353c3039953fa4
Changseo Jang <beleap@beleap.dev>
2024-10-01 14:26:29 +0000

    kubectl-check: add 0.2439.0 bottle.

--- Formula/kubectl-check.rb
+++ Formula/kubectl-check.rb
@@ -6,6 +6,12 @@ class KubectlCheck < Formula
   license "MIT"
   head "https://github.com/BeLeap/kubectl-check.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/BeLeap/homebrew-tap/releases/download/kubectl-check-0.2439.0"
+    sha256 cellar: :any_skip_relocation, ventura:      "68ca89a24ad88469e52011b77f8968e937244a2275669e8095814cb0a8ce6c61"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7cf17fef1e8270ab379f83273e8af86b5d94739c19b972232ab9b0ce439745e6"
+  end
+
   depends_on "rust" => :build
 
   def install
