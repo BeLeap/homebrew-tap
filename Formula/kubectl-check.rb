cca91b8cd0ad43b6bb9b5ef328597d15a70451c7
BeLeap <beleap@beleap.dev>
2024-10-01 23:31:08 +0900

    update version
    Closes #16.
    
    Signed-off-by: Changseo Jang <beleap@beleap.dev>

--- Formula/kubectl-check.rb
+++ Formula/kubectl-check.rb
@@ -1,8 +1,8 @@
 class KubectlCheck < Formula
   desc "Check for kubernetes metadata before executes command"
   homepage "https://github.com/BeLeap/kubectl-check"
-  url "https://github.com/BeLeap/kubectl-check/archive/refs/tags/v0.2439.0.tar.gz"
-  sha256 "c101bd51d950c937025e42315ffa1b40411ec097e30dd035733d4d0887482d67"
+  url "https://github.com/BeLeap/kubectl-check/archive/refs/tags/v0.2439.1.tar.gz"
+  sha256 "5331205b35e1422820c904d06d2300c64336537cca8ee626af12bbf6039ae9bb"
   license "MIT"
   head "https://github.com/BeLeap/kubectl-check.git", branch: "main"
 
