2a28f067c875c402eb2a91281cc24dd0a6823e14
BeLeap <beleap@beleap.dev>
2024-10-01 23:02:42 +0900

    fix: remove comment

--- Formula/kubectl-check.rb
+++ Formula/kubectl-check.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class KubectlCheck < Formula
   desc "Check for kubernetes metadata before executes command"
   homepage "https://github.com/BeLeap/kubectl-check"
@@ -16,15 +13,6 @@ class KubectlCheck < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test kubectl-check`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
     system "false"
   end
 end
