8477390bbfd6b78be00a5bb5c971be880ec9581c
BeLeap <beleap@beleap.dev>
2024-10-03 12:03:39 +0900

    update

--- Formula/kubectl-check.rb
+++ Formula/kubectl-check.rb
@@ -1,8 +1,8 @@
 class KubectlCheck < Formula
   desc "CLI tool to prompt user confirmation before running unsafe kubectl commands"
   homepage "https://github.com/BeLeap/kubectl-check"
-  url "https://github.com/BeLeap/kubectl-check/archive/refs/tags/v0.2439.3.tar.gz"
-  sha256 "c6438ed6099ff66612eefe631a4289a157cde32910b34d666f0b9d5e059dc24f"
+  url "https://github.com/BeLeap/kubectl-check/archive/refs/tags/v0.2439.4.tar.gz"
+  sha256 "6fdd5d10c2e4fef7912dde04c950a81b98d58095983672760f95e1f033fc4f62"
 
   license "MIT"
   head "https://github.com/BeLeap/kubectl-check.git", branch: "main"
